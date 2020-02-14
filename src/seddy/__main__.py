0850ac8849b3e4d76e65be80b2638a178b7d127b
Laurie O <laurie_opperman@hotmail.com>
2020-02-14 11:47:55 +1000

    Add logging-control flags
    Took 15 minutes

--- src/seddy/__main__.py
+++ src/seddy/__main__.py
@@ -2,10 +2,18 @@
 
 import pathlib
 import argparse
+import logging as lg
+
+
+def _setup_logging(verbose: int):  # pragma: no cover
+    level = lg.INFO - (lg.INFO - lg.DEBUG) * verbose
+    fmt = "%(asctime)s [%(levelname)8s] %(name)s: %(message)s"
+    lg.basicConfig(level=level, format=fmt)
 
 
 def _run_app(args: argparse.Namespace):  # pragma: no cover
     from . import app
+    _setup_logging(args.verbose - args.quiet)
     app.run_app(args.decider_json, args.domain, args.task_list)
 
 
@@ -16,6 +24,12 @@ def main():  # pragma: no cover
     )
     parser.add_argument("domain", help="SWF domain")
     parser.add_argument("task_list", help="SWF decider task-list")
+    parser.add_argument(
+        "-v", "--verbose", action="count", default=0, help="increase logging verbosity"
+    )
+    parser.add_argument(
+        "-q", "--quiet", action="count", default=0, help="decrease logging verbosity"
+    )
     args = parser.parse_args()
     _run_app(args)
 
