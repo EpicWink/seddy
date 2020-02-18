539c9ef82979a79109ae1ee6c2c65036828f78eb
Laurie O <laurie_opperman@hotmail.com>
2020-02-18 22:49:42 +1000

    Fix version retrieval

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -1,13 +1,12 @@
 """Configuration file for the Sphinx documentation builder."""
 
-import pathlib
+import pkg_resources
 
 project = "seddy"
 copyright = "2020, SiteSee"
 author = "Laurie O"
 
-_version_path = pathlib.Path(__file__).parent.parent.parent / "VERSION"
-release = _version_path.read_text().strip()  # full version
+release = pkg_resources.get_distribution("seddy").version  # full version
 version = ".".join(release.split(".")[:2])  # short version
 
 extensions = [
